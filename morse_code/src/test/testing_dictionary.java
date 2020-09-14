c37cc2a6efc1ee205fd582eec9704296ebe6ae68
sassolig <gabriellasassoli@gmail.com>
2020-09-14 21:38:47 +0100

    starting new Java Project with TDD approach

---
+++ morse_code/src/test/testing_dictionary.java
@@ -0,0 +1,17 @@
+package test;
+import org.junit.jupiter.api.Test;
+
+
+public class ApplicationTest {
+
+    @Test
+    void test_dictionary{
+        //given
+        //when
+        //then
+
+    }
+
+    @Test
+    void test_
+}

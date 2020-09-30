978840a5a412e6bb3948fa5049bcaa9d311c45e2
sassolig <gabriellasassoli@gmail.com>
2020-09-30 22:36:04 +0100

    finished first bonus challenge

---
+++ morse/src/main/java/Morse/FindWord.java
@@ -0,0 +1,14 @@
+package Morse;
+
+public class FindMorseWithNWords {
+    private int numberOfWords;
+    private String moreseWord;
+
+    public FindMorseWithNWords(int numberOfWords){
+        this.numberOfWords = numberOfWords;
+    }
+
+    public String getMorseWord(){
+
+    }
+}
